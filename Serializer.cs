a186afdb0314ac6f9998a13a3f2ce1a0c2d7f47c
bohlenius <vilgot.bohlenius@gmail.com>
2024-02-13 12:13:13 +0100

    Add : Recursive serialization/ deserialization for nested types

--- Serializer.cs
+++ Serializer.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic.FileIO;
 using System;
 
 namespace Fish.Serialization
@@ -15,43 +16,45 @@ namespace Fish.Serialization
         /// <returns>The size of the instance of the object in bytes</returns>
         static int SerializedSizeOf(Type type, object obj)
         {
-            // Doesnt check for infinite recursion, should fix asap
+            return SerializedSizeOfRecursion(type, obj);
 
-            const byte FIELD_TYPE_HEADER_SIZE = 1;
-            const byte FIELD_SIZE_HEADER_SIZE = 4;
-
-            return FIELD_TYPE_HEADER_SIZE + type switch
+            static int SerializedSizeOfRecursion(Type type, object obj)
             {
-                var t when t == typeof(byte) => sizeof(byte),
-                var t when t == typeof(short) => sizeof(short),
-                var t when t == typeof(ushort) => sizeof(ushort),
-                var t when t == typeof(int) => sizeof(int),
-                var t when t == typeof(uint) => sizeof(uint),
-                var t when t == typeof(long) => sizeof(long),
-                var t when t == typeof(ulong) => sizeof(ulong),
-                var t when t == typeof(float) => sizeof(float),
-                var t when t == typeof(double) => sizeof(double),
-                var t when t == typeof(string) => SizeOfString((string)obj),
-                _ => SizeOfObject(type, obj)
-            };
+                const byte FIELD_SIZE_HEADER_SIZE = 4;
 
-            int SizeOfString(string str)
-            {
-                return FIELD_SIZE_HEADER_SIZE + Buffer.encoding.GetByteCount(str);
-            }
-
-            int SizeOfObject(Type type, object obj)
-            {
-                int size = 0;
-
-                var fields = type.GetFields();
-
-                foreach (var field in fields)
+                return type switch
                 {
-                    size += SerializedSizeOf(field.FieldType, field.GetValue(obj));
+                    var t when t == typeof(byte) => sizeof(byte),
+                    var t when t == typeof(short) => sizeof(short),
+                    var t when t == typeof(ushort) => sizeof(ushort),
+                    var t when t == typeof(int) => sizeof(int),
+                    var t when t == typeof(uint) => sizeof(uint),
+                    var t when t == typeof(long) => sizeof(long),
+                    var t when t == typeof(ulong) => sizeof(ulong),
+                    var t when t == typeof(float) => sizeof(float),
+                    var t when t == typeof(double) => sizeof(double),
+                    var t when t == typeof(string) => SizeOfString((string)obj),
+                    _ => SizeOfObject(type, obj)
+                };
+
+                int SizeOfString(string str)
+                {
+                    return FIELD_SIZE_HEADER_SIZE + Buffer.encoding.GetByteCount(str);
                 }
 
-                return size;
+                int SizeOfObject(Type type, object obj)
+                {
+                    int size = 0;
+
+                    var fields = type.GetFields();
+
+                    foreach (var field in fields)
+                    {
+                        size += SerializedSizeOfRecursion(field.FieldType, field.GetValue(obj));
+                    }
+
+                    return size;
+                }
             }
         }
 
@@ -59,56 +62,97 @@ namespace Fish.Serialization
         /// Serializes a c# object to a byte array
         /// </summary>
         /// <typeparam name="T">The type of the object to serialize</typeparam>
-        /// <param name="obj">The instance of the object to serialize</param>
+        /// <param name="instance">The instance of the object to serialize</param>
         /// <param name="outBuffer">The buffer containing the serialized object</param>
         /// <returns>True on success and false on failure</returns>
-        public static bool Serialize<T>(T obj, out byte[] outBuffer, int prefix = 0, int suffix = 0)
+        public static bool Serialize<T>(T instance, out byte[] outBuffer, int prefix = 0, int suffix = 0)
         {
-            int outBufferSize = SerializedSizeOf(typeof(T), obj) + prefix + suffix;
-            outBuffer = new byte[outBufferSize];
+            int size = SerializedSizeOf(typeof(T), instance) + prefix + suffix;
+            outBuffer = new byte[size];
 
             Buffer buffer = outBuffer;
             buffer.SetPosition(prefix);
 
-            var fields = typeof(T).GetFields();
+            return SerializeRecursion(instance, typeof(T), buffer);
 
-            foreach (var field in fields)
+            static bool SerializeRecursion(object obj, Type type, Buffer buffer, int depth = 0)
             {
-                if (!buffer.Write(field.GetValue(obj), field.FieldType))
-                    return false;
-            }
+                var fields = type.GetFields();
 
-            return true;
+                foreach (var field in fields)
+                {
+                    var fieldType = field.FieldType;
+                    object? fieldValue = field.GetValue(obj);
+
+                    if (fieldValue is null)
+                        return false;
+
+                    // If 'buffer.Write' returns false the type of the current field is not a base type
+                    // therefore it needs to be serialized before writing
+                    if (!buffer.Write(fieldValue, fieldType))
+                        SerializeRecursion(fieldValue, fieldType, buffer, depth + 1);          
+                }
+
+                return true;
+            }
         }
 
         /// <summary>
-        /// Deserializes a byte array into a c# object
+        /// Recursively deserializes a byte array into a c# object
         /// </summary>
         /// <typeparam name="T">The type of the object</typeparam>
-        /// <param name="obj">The object to hold the deserialized data</param>
-        /// <param name="inBuffer">The buffer to Deserialize</param>
+        /// <param name="instance">The instance which will be created from the serialized data</param>
+        /// <param name="inBuffer">The buffer to deserialize</param>
         /// <returns>True on success and false on failure</returns>
-        public static bool Deserialize<T>(out T obj, byte[] inBuffer, int prefix = 0)
+        public static bool Deserialize<T>(out T instance, byte[] inBuffer, int prefix = 0)
             where T : new()
         {
-            obj = new();
-
-            var reference = __makeref(obj);
-
             Buffer buffer = inBuffer;
             buffer.SetPosition(prefix);
 
-            var fields = typeof(T).GetFields();
+            if (!DeserializeRecursion(out object obj, typeof(T), buffer))
+                return false;
 
-            foreach (var field in fields)
-            {
-                if (!buffer.Read(out object value, field.FieldType))
-                    return false;
-
-                field.SetValueDirect(reference, value);
-            }
+            instance = (T)obj;
 
             return true;
+
+            static bool DeserializeRecursion(out object obj, Type type, Buffer buffer, int depth = 0)
+            {
+                if (!NewInstanceOf(out obj, type))
+                    return false;
+
+                var reference = __makeref(obj);
+
+                var fields = type.GetFields();
+                
+                foreach(var field in fields)
+                {
+                    var fieldType = field.FieldType;
+                    object fieldValue;
+
+                    // If 'buffer.Read returns' false the type of the current field is not a base type
+                    // therefore it needs to be deserialized before assignement
+                    if (!buffer.Read(out fieldValue, fieldType))
+                        DeserializeRecursion(out fieldValue, fieldType, buffer, depth + 1);
+
+                    field.SetValueDirect(reference, fieldValue);
+                }
+
+                return true;
+            }
+
+            /// <summary>
+            /// Creates a new instance of an object
+            /// </summary>
+            /// <param name="obj">The new instance of the object</param>
+            /// <param name="type">The type of the object</param>
+            /// <returns>True on success and false on failure</returns>
+            static bool NewInstanceOf(out object obj, Type type)
+            {
+                obj = Activator.CreateInstance(type);
+                return obj is not null;
+            }
         }
     }
 }
